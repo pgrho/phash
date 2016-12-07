be94a0af9d49e34c7b98ee4e09d454989726dcb4
pgrho <shipwreck@live.jp>
2016-12-07 11:19:14 +0900

    Added C# Project and Rename .c files as .cs

---
+++ src/Shipwreck.Phash/phcomplex.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipwreck.Phash
+{
+    // phcomplexd.h
+
+    //# include <stdlib.h>
+    //# include <math.h>
+
+    //# ifndef _PH_COMPLEX_H
+    //#define _PH_COMPLEX_H
+
+    //typedef struct phcomplex
+    //{
+    //    double re;
+    //    double im;
+    //}
+    //Complexd;
+
+    //Complexd polar_to_complex(const double r, const double theta);
+
+    //Complexd add_complex(const Complexd a, const Complexd b);
+
+    //Complexd sub_complex(const Complexd a, const Complexd b);
+
+    //Complexd mult_complex(const Complexd a, const Complexd b);
+
+    //#endif
+
+
+    // phcomplexd.c
+
+    //# include "phcomplex.h"
+
+    //Complexd polar_to_complex(const double r, const double theta)
+    //{
+    //    Complexd result;
+    //    result.re = r * cos(theta);
+    //    result.im = r * sin(theta);
+    //    return result;
+    //}
+    //Complexd add_complex(const Complexd a, const Complexd b)
+    //{
+    //    Complexd result;
+    //    result.re = a.re + b.re;
+    //    result.im = a.im + b.im;
+    //    return result;
+    //}
+    //Complexd sub_complex(const Complexd a, const Complexd b)
+    //{
+    //    Complexd result;
+    //    result.re = a.re - b.re;
+    //    result.im = a.im - b.im;
+    //    return result;
+    //}
+    //Complexd mult_complex(const Complexd a, const Complexd b)
+    //{
+    //    Complexd result;
+    //    result.re = (a.re * b.re) - (a.im * b.im);
+    //    result.im = (a.re * b.im) + (a.im * b.re);
+    //    return result;
+    //} 
+}
