3f5397a517c3b0866350c41992d7eb3bbdc0a58f
pgrho <shipwreck@live.jp>
2016-12-08 14:17:10 +0900

    Modified pack.dat

--- build/pack.bat
+++ build/pack.bat
@@ -1,7 +1,7 @@
 "%~dp0nuget.exe" pack^
  "%~dp0..\src\Shipwreck.Phash\Shipwreck.Phash.csproj"^
- -OutputDirectory "%~dp0..\src\Shipwreck.Phash\bin\Release"
+ -OutputDirectory "%~dp0..\src\Shipwreck.Phash\bin\Release" -IncludeReferencedProjects -Build -Properties Configuration=Release
 
 "%~dp0nuget.exe" pack^
  "%~dp0..\src\Shipwreck.Phash.Data\Shipwreck.Phash.Data.csproj"^
- -OutputDirectory "%~dp0..\src\Shipwreck.Phash.Data\bin\Release"
+ -OutputDirectory "%~dp0..\src\Shipwreck.Phash.Data\bin\Release" -IncludeReferencedProjects -Build -Properties Configuration=Release
