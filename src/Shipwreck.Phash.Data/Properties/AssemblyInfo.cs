db7f8fd21e617b17c74fc8fa04a7772feeef2fa9
pgrho <shipwreck@live.jp>
2016-12-08 13:56:08 +0900

    Modified nuspecs

--- src/Shipwreck.Phash.Data/Properties/AssemblyInfo.cs
+++ src/Shipwreck.Phash.Data/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Shipwreck.Phash.Data")]
+[assembly: AssemblyProduct("priHash DB Integrations")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
