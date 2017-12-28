fd36cb81f17ea1674763f07f1dfe71abb0a6f463
Ruben Aguilar <ThatRubenAguilar@gmail.com>
2017-12-27 22:52:11 -0800

    [operations] Move GetOperations to IByteImageWrapper for performance increase

--- src/Shipwreck.Phash/Imaging/ByteImageHelper.cs
+++ src/Shipwreck.Phash/Imaging/ByteImageHelper.cs
@@ -10,23 +10,12 @@ namespace Shipwreck.Phash.Imaging
             => image as IByteImageWrapper
                 ?? (image as IByteImageWrapperProvider)?.GetWrapper()
                 ?? new GenericByteImageWrapper(image);
-
-        static ConcurrentDictionary<Type, IByteImageOperations> typeOperationsCache = 
-            new ConcurrentDictionary<Type, IByteImageOperations>();
-
-        internal static IByteImageOperations GetOperations(IByteImageWrapper image)
-            => typeOperationsCache.GetOrAdd(image.GetType(), CreateByteImageOperationsForImageType);
-
-        static IByteImageOperations CreateByteImageOperationsForImageType(Type imageType)
-        {
-            return (IByteImageOperations)Activator.CreateInstance(typeof(ByteImageOperations<>).MakeGenericType(imageType));
-        }
-
+        
         public static FloatImage Convolve(this IByteImage image, FloatImage kernel)
             => image.Wrap().Convolve(kernel);
 
         internal static FloatImage Convolve(this IByteImageWrapper image, FloatImage kernel)
-            => GetOperations(image).Convolve(image, kernel);
+            => image.GetOperations().Convolve(image, kernel);
 
         public static FloatImage Blur(this IByteImage image, double sigma)
             => image.Convolve(FloatImage.CreateGaussian(3, sigma));
