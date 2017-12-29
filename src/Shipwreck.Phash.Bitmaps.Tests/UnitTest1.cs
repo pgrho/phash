e5a5acedc23f7e44b235d547d84fa7ac13d74572
Ruben Aguilar <ThatRubenAguilar@gmail.com>
2017-12-28 21:44:11 -0800

    [bitmaps_tests] Add bitmap.tests project and test data

---
+++ src/Shipwreck.Phash.Bitmaps.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shipwreck.Phash.Bitmaps.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
