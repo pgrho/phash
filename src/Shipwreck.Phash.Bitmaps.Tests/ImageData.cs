0d0f94f56cdaf93b2a6f6e8ec27688a19e550798
Ruben Aguilar <ThatRubenAguilar@gmail.com>
2017-12-30 21:32:03 -0800

    Bitmaps tests (#12)
    * [bitmaps_tests] Add bitmap.tests project and test data
    
    * [bitmaps_tests] Add tests for bitmap, bitmapsource, rawbitmapdata matching, small image hashing, and subsection hashing
    
    * [bitmaps_bugfix] Fix normalizing absolute coordinates to relative coordinates in PerPixelInArea
    
    * [bitmaps_bugfix] Bump Phash.Bitmaps version to 0.2.1
    
    * [bitmaps_tests] Use test data which we have rights to
    
    * [bitmaps_tests] Convert Bitmaps.Tests to sdk project
    
    * [bitmaps_tests] Convert mstest to xunit
    
    * [bitmaps_tests] Include copyright free test data

---
+++ src/Shipwreck.Phash.Bitmaps.Tests/ImageData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace Shipwreck.Phash.Bitmaps.Tests
+{
+
+    public class ImageData : IDisposable
+    {
+        static string GetDataDirectory(string directoryName)
+        {
+            return Path.GetFullPath($"./{directoryName}/");
+        }
+
+        static Uri blurredUri = new Uri(GetDataDirectory("blur"));
+        static Uri compressedUri = new Uri(GetDataDirectory("compr"));
+        static Uri subsectionUri = new Uri(GetDataDirectory("subsection"));
+        
+        public LazyBitmap StainedGlassBlurred { get; private set; }
+        public LazyBitmap StainedGlassSmallBlurred { get; private set; }
+
+        public LazyBitmap StainedGlassCompressed { get; private set; }
+
+        public LazyBitmap StainedGlassLeftBottom { get; private set; }
+        public LazyBitmap StainedGlassLeftTop { get; private set; }
+        public LazyBitmap StainedGlassRightBottom { get; private set; }
+        public LazyBitmap StainedGlassRightTop { get; private set; }
+
+        public ImageData()
+        {
+            InitBlurred();
+            InitCompressed();
+            InitSubsection();
+        }
+
+        void InitBlurred()
+        {
+            StainedGlassBlurred = new LazyBitmap(new Uri(blurredUri, "stain_glass.bmp"));
+            StainedGlassSmallBlurred = new LazyBitmap(new Uri(blurredUri, "stain_glass.small.bmp"));
+        }
+
+        void InitCompressed()
+        {
+            StainedGlassCompressed = new LazyBitmap(new Uri(compressedUri, "stain_glass.jpg"));
+        }
+
+        void InitSubsection()
+        {
+            StainedGlassLeftBottom = new LazyBitmap(new Uri(subsectionUri, "stain_glass.left60bottom80.bmp"));
+            StainedGlassLeftTop = new LazyBitmap(new Uri(subsectionUri, "stain_glass.left60top80.bmp"));
+            StainedGlassRightBottom = new LazyBitmap(new Uri(subsectionUri, "stain_glass.right60bottom80.bmp"));
+            StainedGlassRightTop = new LazyBitmap(new Uri(subsectionUri, "stain_glass.right60top80.bmp"));
+        }
+
+        public void Dispose()
+        {
+            StainedGlassBlurred.Dispose();
+            StainedGlassSmallBlurred.Dispose();
+
+            StainedGlassCompressed.Dispose();
+
+            StainedGlassLeftBottom.Dispose();
+            StainedGlassLeftTop.Dispose();
+            StainedGlassRightBottom.Dispose();
+            StainedGlassRightTop.Dispose();
+        }
+    }
+}
---
+++ src/Shipwreck.Phash.Bitmaps.Tests/RawBitmapDataSpecs.cs
@@ -0,0 +1,141 @@
+﻿using Shipwreck.Phash.PresentationCore;
+using System;
+using System.Drawing;
+using System.Linq;
+using Xunit;
+
+namespace Shipwreck.Phash.Bitmaps.Tests
+{
+    
+    public class RawBitmapDataSpecs : IClassFixture<ImageData>
+    {
+        public RawBitmapDataSpecs(ImageData images)
+        {
+            Images = images;
+        }
+
+        public ImageData Images { get; private set; }
+
+        const double ExactThreshhold = 0.000000000000000000000000001;
+
+        // TODO: Categories
+        [Fact]
+        public void Digest_Should_Have_Non_Zero_Hash_For_Small_Images()
+        {
+            var rawBitmapDataDigest = ImagePhash.ComputeDigest(Images.StainedGlassSmallBlurred.Bitmap.ToRawBitmapData().ToLuminanceImage());
+            Assert.Contains(rawBitmapDataDigest.Coefficents, coeff => coeff > 0);
+        }
+
+        [Fact]
+        public void Digest_Should_Match_BitmapSource_Exactly()
+        {
+            var rawBitmapDataDigest = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.Bitmap.ToRawBitmapData().ToLuminanceImage());
+            var bitmapSourceDigest = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.BitmapSource.ToLuminanceImage());
+            Digest_Should_Match_Exactly(rawBitmapDataDigest, bitmapSourceDigest);
+        }
+
+        void Digest_Should_Match_Exactly(Digest one, Digest two)
+        {
+            Assert.Equal(one.ToString(), two.ToString());
+        }
+
+
+        [Fact]
+        public void Digest_Should_Match_Bitmap_Exactly()
+        {
+            var rawBitmapDataDigest = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.Bitmap.ToRawBitmapData().ToLuminanceImage());
+            var bitmapDigest = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.Bitmap.ToLuminanceImage());
+            Digest_Should_Match_Exactly(rawBitmapDataDigest, bitmapDigest);
+        }
+
+
+        [Fact]
+        public void CCR_Should_Match_BitmapSource_Exactly()
+        {
+            var rawBitmapDataDigestBlurred = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.Bitmap.ToRawBitmapData().ToLuminanceImage());
+            var rawBitmapDataDigestCompressed = ImagePhash.ComputeDigest(Images.StainedGlassCompressed.Bitmap.ToRawBitmapData().ToLuminanceImage());
+            var bitmapSourceDigestBlurred = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.BitmapSource.ToLuminanceImage());
+            var bitmapSourceDigestCompressed = ImagePhash.ComputeDigest(Images.StainedGlassCompressed.BitmapSource.ToLuminanceImage());
+
+            CCR_Should_Match_Exactly(
+                new Tuple<Digest, Digest>(rawBitmapDataDigestBlurred, rawBitmapDataDigestCompressed),
+                new Tuple<Digest, Digest>(bitmapSourceDigestBlurred, bitmapSourceDigestCompressed));
+        }
+        
+        void CCR_Should_Match_Exactly(Tuple<Digest, Digest> ccrComparisonOne, Tuple<Digest, Digest> ccrComparisonTwo)
+        {
+            var ccrOne = ImagePhash.GetCrossCorrelation(ccrComparisonOne.Item1, ccrComparisonOne.Item2);
+            var ccrTwo = ImagePhash.GetCrossCorrelation(ccrComparisonTwo.Item1, ccrComparisonTwo.Item2);
+
+            Assert.True(Math.Abs(ccrOne - ccrTwo) < ExactThreshhold);
+        }
+
+
+        [Fact]
+        public void CCR_Should_Match_Bitmap_Exactly()
+        {
+            var rawBitmapDataDigestBlurred = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.Bitmap.ToRawBitmapData().ToLuminanceImage());
+            var rawBitmapDataDigestCompressed = ImagePhash.ComputeDigest(Images.StainedGlassCompressed.Bitmap.ToRawBitmapData().ToLuminanceImage());
+            var bitmapDigestBlurred = ImagePhash.ComputeDigest(Images.StainedGlassBlurred.Bitmap.ToLuminanceImage());
+            var bitmapDigestCompressed = ImagePhash.ComputeDigest(Images.StainedGlassCompressed.Bitmap.ToLuminanceImage());
+
+            CCR_Should_Match_Exactly(
+                new Tuple<Digest, Digest>(rawBitmapDataDigestBlurred, rawBitmapDataDigestCompressed),
+                new Tuple<Digest, Digest>(bitmapDigestBlurred, bitmapDigestCompressed));
+        }
+
+
+        [Fact]
+        public void Left_Bottom_Area_Should_Match_Original_Digest_Exactly()
+        {
+            var originalImage = Images.StainedGlassBlurred.Bitmap.ToRawBitmapData();
+            var rawBitmapDataDigestBlurred = ImagePhash.ComputeDigest(originalImage.ToLuminanceImage());
+
+            var trimArea = new Rectangle(60, 0, originalImage.PixelWidth, originalImage.PixelHeight);
+            var rawBitmapDataDigestLeftBottom = ImagePhash.ComputeDigest(Images.StainedGlassLeftBottom.Bitmap.ToRawBitmapData().ToLuminanceImage(trimArea));
+
+            Area_Should_Match_Original_Digest_Exactly(rawBitmapDataDigestBlurred, rawBitmapDataDigestLeftBottom);
+        }
+
+        void Area_Should_Match_Original_Digest_Exactly(Digest original, Digest subsection)
+        {
+            Assert.Equal(original.ToString(), subsection.ToString());
+        }
+
+        [Fact]
+        public void Left_Top_Area_Should_Match_Original_Digest_Exactly()
+        {
+            var originalImage = Images.StainedGlassBlurred.Bitmap.ToRawBitmapData();
+            var rawBitmapDataDigestBlurred = ImagePhash.ComputeDigest(originalImage.ToLuminanceImage());
+
+            var trimArea = new Rectangle(60, 80, originalImage.PixelWidth, originalImage.PixelHeight);
+            var rawBitmapDataDigestLeftTop = ImagePhash.ComputeDigest(Images.StainedGlassLeftTop.Bitmap.ToRawBitmapData().ToLuminanceImage(trimArea));
+
+            Area_Should_Match_Original_Digest_Exactly(rawBitmapDataDigestBlurred, rawBitmapDataDigestLeftTop);
+        }
+
+        [Fact]
+        public void Right_Bottom_Area_Should_Match_Original_Digest_Exactly()
+        {
+            var originalImage = Images.StainedGlassBlurred.Bitmap.ToRawBitmapData();
+            var rawBitmapDataDigestBlurred = ImagePhash.ComputeDigest(originalImage.ToLuminanceImage());
+
+            var trimArea = new Rectangle(0, 0, originalImage.PixelWidth, originalImage.PixelHeight);
+            var rawBitmapDataDigestRightBottom = ImagePhash.ComputeDigest(Images.StainedGlassRightBottom.Bitmap.ToRawBitmapData().ToLuminanceImage(trimArea));
+
+            Area_Should_Match_Original_Digest_Exactly(rawBitmapDataDigestBlurred, rawBitmapDataDigestRightBottom);
+        }
+
+        [Fact]
+        public void Right_Top_Area_Should_Match_Original_Digest_Exactly()
+        {
+            var originalImage = Images.StainedGlassBlurred.Bitmap.ToRawBitmapData();
+            var rawBitmapDataDigestBlurred = ImagePhash.ComputeDigest(originalImage.ToLuminanceImage());
+
+            var trimArea = new Rectangle(0, 80, originalImage.PixelWidth, originalImage.PixelHeight);
+            var rawBitmapDataDigestRightTop = ImagePhash.ComputeDigest(Images.StainedGlassRightTop.Bitmap.ToRawBitmapData().ToLuminanceImage(trimArea));
+
+            Area_Should_Match_Original_Digest_Exactly(rawBitmapDataDigestBlurred, rawBitmapDataDigestRightTop);
+        }
+    }
+}
