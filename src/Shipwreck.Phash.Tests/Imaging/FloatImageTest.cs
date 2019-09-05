27fb0efd94cde4694aab586d4f3be01cc268a7fb
pgrho <shipwreck@live.jp>
2019-09-05 15:50:54 +0900

    Vectorize FloatImage.MatrixMultiply

--- src/Shipwreck.Phash.Tests/Imaging/FloatImageTest.cs
+++ src/Shipwreck.Phash.Tests/Imaging/FloatImageTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Linq;
+using System.Numerics;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -6,8 +7,8 @@ namespace Shipwreck.Phash.Imaging
 {
     public class FloatImageTest
     {
-
         private readonly ITestOutputHelper _Output;
+
         public FloatImageTest(ITestOutputHelper output)
         {
             _Output = output;
@@ -111,6 +112,39 @@ namespace Shipwreck.Phash.Imaging
                     Assert.Equal(src1[x, y] * src2[x, y], res[x, y], 7);
                 }
             }
+        }
+
+        [Theory]
+        [InlineData(1, false)]
+        [InlineData(3, false)]
+        [InlineData(4, false)]
+        [InlineData(5, false)]
+        [InlineData(7, false)]
+        [InlineData(8, false)]
+        [InlineData(9, false)]
+        [InlineData(1, true)]
+        [InlineData(3, true)]
+        [InlineData(4, true)]
+        [InlineData(5, true)]
+        [InlineData(7, true)]
+        [InlineData(8, true)]
+        [InlineData(9, true)]
+        public void MatrixMultiplyTest(int width, bool transpose)
+        {
+            _Output.WriteLine("Vector<float>.Count: {0}", Vector<float>.Count);
+            _Output.WriteLine("Vector.IsHardwareAccelerated: {0}", Vector.IsHardwareAccelerated);
+
+            var src = new FloatImage(width, 1);
+            var src2 = transpose ? new FloatImage(width, 1) : new FloatImage(1, width);
+            for (var i = 0; i < src.Array.Length; i++)
+            {
+                src.Array[i] = i + 1;
+                src2.Array[i] = 2 * i;
+            }
+
+            var dest = src.MatrixMultiply(src2, isTransposed: transpose);
+
+            Assert.Equal(Enumerable.Range(0, width).Sum(i => (i + 1) * 2 * i), dest[0, 0]);
         }
     }
 }
